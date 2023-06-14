1f2b218b156e1d38951f4575dfe84a83e7a9890f
HSC <HSC@heinen-automation.de>
2023-06-14 14:42:19 +0200

    Add a DrawingView example

--- MauiRepro/MainPage.xaml.cs
+++ MauiRepro/MainPage.xaml.cs
@@ -1,24 +1,34 @@
-﻿namespace MauiRepro;
+﻿using CommunityToolkit.Maui.Core.Views;
+
+namespace MauiRepro;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	async void Save_Clicked(object sender, EventArgs e) {
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		float w = (float)Canvas.Width;
+		float h = (float)Canvas.Height;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		// Draw a line across the canvas or else it gets cropped to the used part of it
+		DrawingLine dl = new();
+		dl.LineColor = Colors.Transparent;
+		dl.LineWidth = 0;
+		dl.Points.Add(new(0, 0));
+		dl.Points.Add(new(w, h));
+		Canvas.Lines.Add(dl);
+		
+		using Stream stream = await Canvas.GetImageStream(w, h);
+		MemoryStream ms = new();
+		stream.CopyTo(ms);
+		//await File.WriteAllBytesAsync(Android.App.Application.Context.GetExternalFilesDir(null) + "/test.bmp", ms.ToArray());
+		ms.Position = 0;
+		imgSaved.Source = ImageSource.FromStream(() => ms);
 	}
 }
 
